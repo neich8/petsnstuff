17a95cfe213714fa8d0bdbcfe3facae2fa6b96b5
Matt Berns <matthewl.berns@gmail.com>
2014-05-25 13:45:15 -0500

    added untracked model files

---
+++ petsnstuff/models/nutrition.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var nutritionSchema = new Schema ({
+  Brand: String,
+  Method: String,
+  Protein: Number,
+  Fat: Number,
+  Carbs: Number
+});
+
+
+module.exports = mongoose.model('Nutrition', nutritionSchema);

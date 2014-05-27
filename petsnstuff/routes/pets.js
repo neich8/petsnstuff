bf7b09426e9202f4160a84c3ff99761a99ad8917
neich8 <neich0888@gmail.com>
2014-05-26 19:17:36 -0500

    added dynamic tabs for pets on profile page

--- petsnstuff/routes/pets.js
+++ petsnstuff/routes/pets.js
@@ -1,28 +1,51 @@
 var User = require("../models/user")["User"];
 var Pet = require("../models/user")["Pet"];
+var Shot = require("../models/user")["Shot"];
+
 module.exports = function(app) {
-	app.post('/create/:id', function(req, res) {
-		var id = req.params.id
-		var pet = {
-			name: req.body.name,
-			age: req.body.age,
-			shots: req.body.shots,
-			weight: req.body.weight,
-			type: req.body.type,
-			photo: req.body.photo
+	app.post('/create/:data', function(req, res) {
+		var id = req.session.userid
+		var form_data = JSON.parse(req.params.data)
+		var name = form_data[0].value
+		var age = form_data[1].value
+		var weight = form_data[2].value
+		var licensenumber = form_data[3].value
+		var type = form_data[4].value
+		var markings = form_data[5].value
+		var shotname = form_data[6].value
+		var shotdate = form_data[7].value
+		var photo = "test.png"
+
+		var shot = {
+      shotName: shotname,
+      examDate: shotdate
 		}
 
-		// console.log(pet)
+		var pet = {
+			name: name,
+			age: age,
+			weight: weight,
+			license: licensenumber,
+			breed: type,
+			shots: shot,
+			markings: markings,
+			photo: photo
+		}
+
 		User.find({_id: id}, function(err, users) {
 			if(err) {
-				console.log("Shits broke yo")
+				console.log("Cannot find pets")
 			}
 			else {
 				console.log(users[0])
 				user = users[0]
 				user.pets.push(pet)
+				user.pets[user.pets.length-1].shots.push(shot)
 				user.save(function(err, user) {})
-				res.redirect("/profile/" + id)
+				console.log(users[0])
+				res.send({pet: pet})
+
+				//res.redirect("/profile/" + id)
 			}
 		})
 	});

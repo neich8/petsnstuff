f969357af55817ce05b6e55d89a4e774c9ebe397
MLBerns <matthewl.berns@gmail.com>
2014-05-29 17:52:16 -0500

    Delete config.js

--- petsnstuff/config.js
+++
@@ -1,8 +0,0 @@
-var config =  {
-FACEBOOK_APP_ID:"249952498528444",
-FACEBOOK_APP_SECRET: "6f35c61cdc85d941f798b71b2b3c660a",
-MONGOOSECONNECT: 'mongodb://localhost:27017/petsnstuff',
-CALLBACK: "http://localhost:3000/auth/facebook/callback"
-}
-
-module.exports = config;
